2e38bd9c89b67a85227994683fd18f14296ece63
alexidr9116 <alexdr9116@gmail.com>
2023-03-25 06:16:32 +0200

    #event detail page

--- src/components/cards/EventTicketDetailCard.js
+++ src/components/cards/EventTicketDetailCard.js
@@ -2,31 +2,38 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { fDescriptionTypeDate, fToNow } from "../../utils/formatTime";
 import GradientBorderWrapper from "../wrappers/GradientBorderWrapper";
 
-export default function EventTicketDetailCard({ ticket, onAction, eventType }) {
-
+export default function EventTicketDetailCard({ ticket, onAction, eventType,  }) {
   const onTap = () => {
     if (onAction) {
       onAction();
     }
   };
   return (
-    <div className={`w-full ${ticket.sold?'opacity-60':'cursor-pointer '} `} onClick={onTap}>
+    <div
+      className={`w-full ${ticket.sold ? "opacity-60" : "cursor-pointer "} `}
+      onClick={onTap}
+    >
       <GradientBorderWrapper>
         <div className="flex gap-2 justify-between p-4 items-center ">
           <div className="flex  gap-2">
             <div className="avatar h-12 w-12 flex-1 aspect-square">
-                <LazyLoadImage src = {ticket.avatar} alt="" effect="blur" className="aspect-square  rounded-full "/>
+              <LazyLoadImage
+                src={ticket.avatar}
+                alt=""
+                effect="blur"
+                className="aspect-square  rounded-full "
+              />
             </div>
             <div className="flex flex-col">
-                <h5>{eventType  }</h5>
-                <h6 className="text-stone-500">{fDescriptionTypeDate(ticket.date)}</h6>
-                <h6>{fToNow(ticket.date)}</h6>
+              <h6>{fDescriptionTypeDate(ticket.date)}</h6>
+              <h5 className="text-stone-500">{ticket.position}</h5>
+              <h6>{fToNow(ticket.date)}</h6>
             </div>
           </div>
           <div className="flex gap-2 items-center">
             <div className="flex flex-col">
-                <h5 className=" ">{ticket.price}</h5>
-                <h6 className="text-stone-500 text-sm">/Ingresso</h6>
+              <h5 className=" ">{ticket.price}</h5>
+              <h6 className="text-stone-500 text-sm">/Ingresso</h6>
             </div>
           </div>
         </div>
